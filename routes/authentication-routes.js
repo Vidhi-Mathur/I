5bc8906621e1ef79680fdc6f01db05691749f6b2
Vidhi <mathurvidhi2505@gmail.com>
2024-04-09 15:29:49 +0530

    Added signup route

--- routes/authentication-routes.js
+++ routes/authentication-routes.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const authController = require('../controllers/authentication-controller')
 
+router.post('/signup', authController.postSignup)
+
 router.post('/login', authController.postLogin)
 
 router.post('/logout', authController.postLogout)
