148ad812463f2a475520b28f7f5bb91095931055
Vidhi <mathurvidhi2505@gmail.com>
2024-04-09 15:08:00 +0530

    Improving certain routes protection

--- routes/warehouse-routes.js
+++ routes/warehouse-routes.js
@@ -3,19 +3,21 @@ const router = express.Router()
 const warehouseController = require('../controllers/warehouse-controller')
 const { protectedRoute } = require('../controllers/authentication-controller')
 
-//Create warehouse
-router.post('/new', protectedRoute, warehouseController.createWarehouse)
-
 //Retrieve a single warehouse using Id
 router.get('/:id', warehouseController.getWarehouseById)
 
 //Retrieve all warehouses
 router.get('/', warehouseController.getWarehouses)
 
+router.use(protectedRoute)
+
+//Create warehouse
+router.post('/new', warehouseController.createWarehouse)
+
 //Update warehouse
-router.patch('/:id', protectedRoute, warehouseController.updateWarehouse)
+router.patch('/:id', warehouseController.updateWarehouse)
 
 //Delete warehouse
-router.delete('/:id', protectedRoute, warehouseController.deleteWarehouse)
+router.delete('/:id', warehouseController.deleteWarehouse)
 
 module.exports = router
